f8ad1191f8d7967fae3589124ba2aad13c7f3e6f
user <user@LAPTOP-DBEPHPBU>
2023-02-14 14:08:18 +0900

    [예지] 게시판 공지사항 작성하기 파트 1차

--- teamp/src/main/java/com/example/demo/dao/BoardService.java
+++ teamp/src/main/java/com/example/demo/dao/BoardService.java
@@ -12,5 +12,7 @@ public interface BoardService {
 	// 게시글 상세 출력
 	HashMap<String, Object> detailBoard(HashMap<String, Object> map); // 데이터 호출을 위한 인터페이스	
 	// 게시글 수정
-	void editBoard(HashMap<String, Object> map);
+	void editBoard(HashMap<String, Object> map);	//update
+	// 게시글 추가
+	void insertBoard(HashMap<String, Object> map);	//add / insert
 }
