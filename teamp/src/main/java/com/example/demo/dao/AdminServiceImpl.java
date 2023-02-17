f65fa60bf9c98ef0d05bcc4d36390b11fa7e1369
tj-bu-03 <tj-bu-03@DESKTOP-ENSET2K>
2023-02-17 15:14:24 +0900

    [예지] admin 생성

--- teamp/src/main/java/com/example/demo/dao/AdminServiceImpl.java
+++ teamp/src/main/java/com/example/demo/dao/AdminServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.mapper.AdminMapper;
+import com.example.demo.model.Admin;
 
 
 @Service //서비스임을 선언
@@ -11,6 +15,17 @@ public class AdminServiceImpl implements AdminService{
 
 	@Autowired //Mapper와 연결
 	private AdminMapper adminMapper;
+	
+	// 관리자 일반회원 출력
+	@Override
+	public List<Admin> normalMemList(HashMap<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		int startNum = Integer.parseInt(String.valueOf(map.get("startNum")));
+		int lastNum = Integer.parseInt(String.valueOf(map.get("lastNum")));
+		map.put("startNum", startNum);
+		map.put("lastNum", lastNum);
+		return adminMapper.normalMemList(map);
+	}
 
 
 
--- teamp/src/main/java/com/example/demo/mapper/AdminMapper.java
+++ teamp/src/main/java/com/example/demo/mapper/AdminMapper.java
@@ -1,8 +1,14 @@
 package com.example.demo.mapper;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.model.Admin;
 
 @Mapper
 public interface AdminMapper {
+	// 관리자 일반회원 출력
+	List<Admin> normalMemList(HashMap<String, Object> map) throws Exception;	
 }
