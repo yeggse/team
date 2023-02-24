f3b9270a9879db14564218cf359fb8b52831aa03
tj-bu-03 <tj-bu-03@DESKTOP-ENSET2K>
2023-02-24 09:21:51 +0900

    .

--- teamp/bin/src/main/webapp/js/test.js
+++
@@ -1,3 +0,0 @@
-const donut = document.querySelector('.donut');
-donut.dataset.percent = totalMinwon;
-donut.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon}%, #F2F2F2 ${totalMinwon}% 100%)`;
